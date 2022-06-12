2e03303b71689fde6f8ebfca703a9d513a49b6a4
suchinthv444555 <56825605+suchinthv444555@users.noreply.github.com>
2022-06-12 23:17:44 +0530

    initial commit

---
+++ code/boost_converter/boost_converter.ino
@@ -0,0 +1,26 @@
+int pot =A0;
+int feedback=A1;
+int pwm =3;
+int duty=0;
+
+void setup() {
+
+
+TCCR2B = TCCR2B & B11111000 | B00000001; // for PWM frequency of 31372.55 Hz
+pinMode(pwm,OUTPUT);
+pinMode(pot,INPUT);
+Serial.begin(9600);
+
+// put your setup code here, to run once:
+
+}
+
+void loop() {
+  int voltage = analogRead(pot);
+  int volt= map(voltage,0,1023,0,255);
+  
+analogWrite(pwm,volt);
+// put your main code here, to run repeatedly:
+
+
+}
